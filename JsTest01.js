78a10243261a379ef4fd1eef50a2f2ff2bd91bcd
Edgar Huarcaya Chocce <ehuarcaya@teon.pe>
2020-06-08 22:50:52 -0500

    variables

--- JsTest01.js
+++ JsTest01.js
@@ -1,2 +1,19 @@
-let nombre = "Mi nombre";
+let nombre = 'Ponciano';
+const apellido = 'Huarcaya';
+
+const miFunction = (param1, param2)=>{
+    return `${param1} ${param2}`
+}
+
+var fullName = `Mi apellido es ${apellido} y mi nombre es ${apellido} ${miFunction('Edad 34', 'Pais Peru')}`;
+
+const user = {
+    name: 'Administrador',
+    lastname: 'Apellido',
+    hobbies: ['Futbol','Basquet']
+}
+
+const {name,lastname}= user
+
+console.log(`${name} ${lastname}`);
 
